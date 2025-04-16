b1c9d13fae267abbcbff7e0566ae0e8fe2344b89
Jeremy <jeremyhervo@gmail.com>
2025-04-16 17:37:10 +0200

    fix: retour à la configuration sans basePath

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: process.env.GITHUB_ACTIONS ? '/yamzoning' : '',
   images: {
     unoptimized: true,
   },
