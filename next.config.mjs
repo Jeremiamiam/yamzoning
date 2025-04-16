5eae41c7e83a1ea32c8493b45cae62f39c3d506e
Jeremy <jeremyhervo@gmail.com>
2025-04-16 17:34:24 +0200

    fix: basePath conditionnel selon l'environnement

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: '/yamzoning',
+  basePath: process.env.GITHUB_ACTIONS ? '/yamzoning' : '',
   images: {
     unoptimized: true,
   },
